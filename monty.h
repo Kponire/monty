38ddd742a44138bda0b06c733338f91a43c1042f
kponire <kponire@gmail.com>
2023-12-13 08:26:38 -0800

    monty

--- monty.h
+++ monty.h
@@ -52,13 +52,10 @@ typedef struct global_variable
 } global_var;
 
 extern global_var var_global;
-
 void read_file(char *filename, stack_t **stack);
 char *parse_line(char *line, stack_t **stack, unsigned int line_number);
 typedef void (*instruct_func)(stack_t **stack, unsigned int line_number);
 instruct_func get_op_func(char *str);
-
-/*Fuxntions Monty*/
 void _pall(stack_t **stack, unsigned int line_number);
 void _push(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
@@ -67,19 +64,13 @@ void _swap(stack_t **stack, unsigned int line_number);
 void _nop(stack_t **stack, unsigned int line_number);
 void _rotl(stack_t **stack, unsigned int line_number);
 void _rotr(stack_t **stack, unsigned int line_number);
-
-/*Functions string ascci */
 void _pchar(stack_t **stack, unsigned int line_number);
 void _pstr(stack_t **stack, unsigned int line_number);
-
-/*Functions math*/
 void _sub(stack_t **stack, unsigned int line_number);
 void _add(stack_t **stack, unsigned int line_number);
 void _mul(stack_t **stack, unsigned int line_number);
 void _div(stack_t **stack, unsigned int line_number);
 void _mod(stack_t **stack, unsigned int line_number);
-
-/*Utility functions*/
 void free_stack(stack_t *head);
 void dobfree(stack_t **stack);
 void dobfree2(stack_t **stack);
