4e857ab06b9c3c4267d1a34236266fc9dc3225f1
kponire <kponire@gmail.com>
2023-12-14 03:12:41 -0800

    monty

--- pchar.c
+++ pchar.c
@@ -9,7 +9,7 @@ void _pchar(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		free(var_global.buffer);
 		fclose(var_global.file);
-		free_dlistint(*stack);
+		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
 
@@ -19,7 +19,7 @@ void _pchar(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
 		free(var_global.buffer);
 		fclose(var_global.file);
-		free_dlistint(*stack);
+		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
 
